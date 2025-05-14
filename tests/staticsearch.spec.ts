2a78fd59aaff1ef6f99325d07b402348395eb976
SivaShopup <110815018+SivaShopup@users.noreply.github.com>
2025-05-14 11:39:11 +0530

    staticsearch

--- tests/staticsearch.spec.ts
+++ tests/staticsearch.spec.ts
@@ -22,18 +22,13 @@ test('static data for tshirts', async ({ page }) => {
   await page.fill('#search_box', 'tshirts');
   await page.press('#search_box', 'Enter');
 
-  // Wait for results
-//   await page.waitForSelector('.list_ctr fw lft', {timeout: 5000});
-
-  // Expand brand filter (dynamic)
-  await page.click('.sprite_list');
-  await page.waitForSelector('.filter-brands');
-
   // Select "LuvLap" brand if available
   const BabyhugCheckbox = page.locator('label:has-text("babyhug") input[type="checkbox"]');
   if (await BabyhugCheckbox.isVisible()) {
     await BabyhugCheckbox.check();
-  }
+  } else {
+        console.log('Babyhug brand filter not found or not visible.');
+      }
 
   await page.waitForTimeout(2000); // Adjust if filters reload asynchronously
 
@@ -46,14 +41,14 @@ test('static data for tshirts', async ({ page }) => {
 
   // Pagination: go to page 2
   const nextPage = page.locator('.pagination .page-link', { hasText: '2' });
-  await nextPage.click();
+//   await nextPage.click();
 
-  await page.waitForSelector('.search-list');
+//   await page.waitForSelector('.search-list');
 
   // Click first product
   const product = page.locator('.search-list .product-box').first();
-  await expect(product).toBeVisible();
-  await product.click();
+//   await expect(product).toBeVisible();
+//   await product.click();
 
   // Verify product detail
   const title = page.locator('h1');
